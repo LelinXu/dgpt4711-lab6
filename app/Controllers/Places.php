4295e41a188868e2182d8ac39b0faa806419e22e
lelinXu <1191880563@qq.com>
2020-10-20 15:31:35 +0800

    commit comp4711-lab6(all) finish

---
+++ app/Controllers/Places.php
@@ -0,0 +1,10 @@
+<?php namespace App\Controllers; 
+  class Places extends \CodeIgniter\Controller
+  {
+      public function index() 
+      {
+           $places = new \App\Models\Places(); 
+           $records = $places->findAll(); 
+           return json_encode($records); 
+      }
+  }
